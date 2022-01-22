17a79c855e27901fd923bfd6f7c2a50c69981b85
Varphone Wong <varphone@qq.com>
2022-01-22 17:33:23 +0800

    修正 FV_COMMON_DIR 目录导出不生效的问题
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::env;
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
-    if let Ok(dir) = env::var("CARGO_MANIFEST_DIR") {
-        println!("cargo:env=FV_COMMON_DIR={}", dir);
+    if let Ok(dir) = env::var("FV_COMMON_DIR").or(env::var("CARGO_MANIFEST_DIR")) {
+        println!("cargo:rustc-env=FV_COMMON_DIR={}", dir);
     }
 }
