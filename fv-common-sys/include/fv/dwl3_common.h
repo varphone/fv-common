20dea18f0dcd3c70540831090c4144ecda23a94b
majj <majj@full-v.com>
2023-06-17 17:01:59 +0800

    添加dwl3头文件

---
+++ fv-common-sys/include/fv/dwl3_common.h
@@ -0,0 +1,92 @@
+﻿#ifndef DWL3_COMMON_H
+#define DWL3_COMMON_H
+
+#include <list>
+#include <iostream>
+#include <utility>
+
+namespace dwl3 {
+
+constexpr float PI = 3.1415926f;
+
+/// @brief SEG_TYPE 线段的类型
+enum class SEG_TYPE {
+    LINE,
+    POLYLINE,
+    CIRCLE
+};
+
+template <typename T>
+struct _segment {
+    _segment<T>() {
+        org.x = org.y = 0;
+        order = 0;
+    }
+#if 0
+    _segment<T>(const _segment<T>& data) {
+        this->idxs  = data.idxs;
+        this->idxe  = data.idxe;
+        this->type  = data.type;
+        this->org   = data.org;
+        this->s     = data.s;
+        this->e     = data.e;
+        this->order = data.order;
+        this->r     = data.r;
+        this->theta = data.theta;
+        for (int i = 0; i < 5; i++) {
+            this->coefficient[i] = data.coefficient[i];
+        }
+    }
+    _segment<T>& operator=(const _segment<T>& data) {
+        this->idxs  = data.idxs;
+        this->idxe  = data.idxe;
+        this->type  = data.type;
+        this->org   = data.org;
+        this->s     = data.s;
+        this->e     = data.e;
+        this->order = data.order;
+        this->r     = data.r;
+        this->theta = data.theta;
+        for (int i = 0; i < 5; i++) {
+            this->coefficient[i] = data.coefficient[i];
+        }
+        return *this;
+    }
+#endif
+    typename std::list<T>::const_iterator idxs; /// 线段起始点在队列中的索引
+    typename std::list<T>::const_iterator idxe; /// 线段结束点在队列中的索引
+    SEG_TYPE type;                              /// 线段类型
+    T org;                                      /// 用于记录拟合原点
+    T s;                                        /// 起始点坐标
+    T e;                                        /// 结束点坐标
+    int order;                                  /// 阶层
+    T center;                                   /// 圆心
+    float r;                                    /// 极坐标方程的r
+    float theta;                                /// 极坐标方程的theta，弧度
+    float coefficient[9];                       /// 多项式系数，高阶在高位
+
+    float length() const {
+        float dx = s.x - e.x;
+        float dy = s.y - e.y;
+        float len = sqrtf(dx * dx + dy * dy);
+        return len;
+    }
+
+    bool operator<(const struct _segment<T> &rhs) const {
+        return this->length() < rhs.length();
+    }
+
+    bool operator>(const struct _segment<T> &rhs) const {
+        return this->length() > rhs.length();
+    }
+};
+
+template <typename T>
+using segment = struct _segment<T>;
+
+template <typename T>
+using segmentPair = std::pair<dwl3::segment<T>, dwl3::segment<T>>;
+
+}
+
+#endif
---
+++ fv-common-sys/include/fv/dwl3_interface.h
@@ -0,0 +1,64 @@
+﻿#ifndef DWL3_INTERFACE_H
+#define DWL3_INTERFACE_H
+
+#include <opencv2/core.hpp>
+#include <fv/dwl3_common.h>
+#include <list>
+#include <vector>
+
+namespace dwl3 {
+
+enum class LINETYPE : int {
+    LINE   = 0, /// 直线
+    POLY   = 1, /// 曲线
+    CIRCLE = 2, /// 圆
+    DOME   = 3, /// 凸圆
+    COVE   = 4, /// 凹圆
+};
+
+enum class WELDTYPE : int {
+    N_LEFT         = 0x10, /// N型左
+    N_RIGHT        = 0x11, /// N型右
+    BUTT           = 0x20, /// 对接
+    M              = 0x30, /// M型
+    GROOVE_V       = 0x31, /// V型破口
+    K_LEFT         = 0x32, /// V型破口左侧开坡口
+    K_RIGHT        = 0x33, /// V型破口右侧开坡口
+    IN_FILLET      = 0x40, /// 内角焊
+    OUT_FILLET     = 0x41, /// 外角焊
+    RIPPLE_LEFT    = 0x42, /// 波纹板左弧
+    RIPPLE_RIGHT   = 0x43, /// 波纹板右弧
+    LINE           = 0x50, /// 直线
+    LINE_CIRCLE    = 0x60, /// 直线圆
+    CIRCLE_LINE    = 0x61, /// 圆直线
+    CIRCLE         = 0x70, /// 圆检测
+    CIRCLE_TOP     = 0x80, /// 顶点
+    SOLDER_JOINT   = 0x90, /// 焊点检测
+};
+
+typedef struct _featureInfo {
+    _featureInfo() {
+        this->width  = 0.f;
+        this->height = 0.f;
+        this->angle  = 0.f;
+        this->num    = 0.f;
+        this->pts    = 0.f;
+        this->rangeR = 0.f;
+    }
+
+    float width;
+    float height;
+    float angle;
+    int index; // 选中的索引
+    int num; // feature有效个数
+    cv::Point2f feature[8];
+    std::list<segment<cv::Point2f>> segs;
+    std::vector<float> key;
+    unsigned long long pts;
+    cv::Point2f rangeCenter;
+    float rangeR;
+} featureInfo;
+
+}
+
+#endif
